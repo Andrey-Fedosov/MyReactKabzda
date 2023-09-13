df949aa986e7c53ad374210cd50127caea53b548
Andrey-Fedosov <andrefedosov@gmail.com>
2023-09-13 23:56:47 +0300

    init: start new

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Work with reakt kabzda kak
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Rating } from "./components/Rating";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      Hello
+      <Rating />
+    </div>
+  );
 }
 
 export default App;
